c1cc7a736d35891c008f69f11abc9a00e8044421
paulportilla1 <116098987+Paulportilla1@users.noreply.github.com>
2023-11-20 13:14:05 -0500

    base de datos corregido

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS user1 (
-    id SERIAL,
+    id SERIAL PRIMARY KEY,
     name1 VARCHAR(100) NOT NULL,
     age INT NOT NULL,
     registration VARCHAR(100) NOT NULL,
@@ -7,39 +7,33 @@ CREATE TABLE IF NOT EXISTS user1 (
     );
 
 CREATE TABLE IF NOT EXISTS multimedia (
-    id SERAIl,
+    id SERIAL PRIMARY KEY,
     type1 VARCHAR(100) NOT NULL,
     URL VARCHAR(100) NOT NULL,
     user1_id INT NOT NULL,
-    PRIMARY KEY (id),
     FOREIGN KEY (user1_id) REFERENCES user1(id)
-);
+    );
 
 CREATE TABLE IF NOT EXISTS game (
-    id SERIAL,
+    id SERIAL PRIMARY KEY,
     name1 VARCHAR(100) NOT NULL,
     description VARCHAR(100) NOT NULL,
     user1_id INT NOT NULL,
-    PRIMARY KEY (id),
     FOREIGN KEY (user1_id) REFERENCES user1(id)
-);
-
-CREATE TABLE IF NOT EXISTS progress (
-    id SERAIL,
-    score VARCHAR(100) NOT NULL,
-    dat1 DATE,
-    user1_id INT NOT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY (user1_id) REFERENCES user1(id)
-);
-
-CREATE TABLE IF NOT EXISTS pnotification (
-    id SERIAL,
-    read1 VARCHAR(100) NOT NULL,
-    menssage VARCHAR(100) NOT NULL,
-    progress_id INT NOT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY (progress_id) REFERENCES progress(id)
     );
 
+CREATE TABLE IF NOT EXISTS progress (
+    id SERIAL PRIMARY KEY,
+    score VARCHAR(100) NOT NULL,
+    date1 DATE,
+    user1_id INT NOT NULL,
+    FOREIGN KEY (user1_id) REFERENCES user1(id)
+    );
 
+CREATE TABLE IF NOT EXISTS pnotification (
+    id SERIAL PRIMARY KEY,
+    read1 VARCHAR(100) NOT NULL,
+    message VARCHAR(100) NOT NULL,
+    progress_id INT NOT NULL,
+    FOREIGN KEY (progress_id) REFERENCES progress(id)
+    );
