f3b8a7061e193a32c63732b46fa611b6291eb749
Lesculcay04 <116099796+Lesculcay04@users.noreply.github.com>
2023-12-21 12:38:38 -0500

    Created Repository

--- src/main/kotlin/com/proyecto/piensa/repository/ProgressRepository.kt
+++ src/main/kotlin/com/proyecto/piensa/repository/ProgressRepository.kt
@@ -1,4 +1,11 @@
 package com.proyecto.piensa.repository
 
-interface ProgressRepository {
+import com.proyecto.piensa.model.Notification
+import com.proyecto.piensa.model.Progress
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface ProgressRepository: JpaRepository<Progress, Long?> {
+    fun findById (id: Long?): Progress?
 }
--- src/main/kotlin/com/proyecto/piensa/repository/UserRepository.kt
+++ src/main/kotlin/com/proyecto/piensa/repository/UserRepository.kt
@@ -1,4 +1,11 @@
 package com.proyecto.piensa.repository
 
-interface UserRepository {
+import com.proyecto.piensa.model.Notification
+import com.proyecto.piensa.model.User
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface UserRepository: JpaRepository<User, Long?> {
+    fun findById (id: Long?): User?
 }
